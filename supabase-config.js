ffaeded0feb427321e755413a5dc658fae47d047
LTL322 <danysmsye82@gmail.com>
2026-08-25 00:54:51 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-window.LTL_SUPABASE_URL = '';
-window.LTL_SUPABASE_ANON_KEY = '';
+window.LTL_SUPABASE_URL = 'https://mtvudsyvequmvguzyhiw.supabase.co/';
+window.LTL_SUPABASE_ANON_KEY = 'sb_publishable_bq67S21Ey-yWrx8n7gbTZg_9v_viOlU';
