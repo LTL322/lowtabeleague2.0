f68eb91eed9e476400db3208237e756a0673a3cf
LTL322 <danysmsye82@gmail.com>
2026-08-25 01:21:10 +0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,3 @@
-window.LTL_SUPABASE_URL = 'https://mtvudsyvequmvguzyhiw.supabase.co/';
-window.LTL_SUPABASE_ANON_KEY = 'sb_publishable_bq67S21Ey-yWrx8n7gbTZg_9v_viOlU';
+// Вставь сюда данные из Supabase -> Project Settings -> API Keys
+window.LTL_SUPABASE_URL = 'https://mtvudsyvequmvguzyhiw.supabase.co';
+window.LTL_SUPABASE_ANON_KEY = 'sb_publishable_bq67S21Ey-yWrx8n7gbTZg_9v_viOlU'; // Publishable key / старый anon key
